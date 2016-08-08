2ae9afc37c8150f606ac25aa0a03380acd35a27d
Subhan <sub.metu@gmail.com>
2016-08-08 23:34:24 +0300

    app.js
    main jquery file

---
+++ app.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  $("h1").css("font-size","75px");
+  $("h1,h4").css("font-family","Lucida Bright");
+  $("h1").css("color","green");
+  $("#description").css("font-family","Verdana");
+  $("#quote").css("color","green");
+});
